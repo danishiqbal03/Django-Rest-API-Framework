8c7ccaab3f29498206ce3a5c947ef4599036f531
danishiqbal03 <danishiqbalhasnain@gmail.com>
2024-07-21 03:23:02 +0500

    Authentication and Model Permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,82 +1,93 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, mixins, permissions
-
-from .models import Product
-from .serializers import ProductSerializer
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.shortcuts import get_object_or_404
+from .models import Product
+from .permissions import IsStaffPermission
+from .serializers import ProductSerializer
 
 
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-    #if you wanna add additional func to after passing through serializer
+
+    # if you wanna add additional func to after passing through serializer
     def perform_create(self, serializer):
-        title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content') or None
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
         if content is None:
             content = title
         serializer.save(content=content)
+
 
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
 
+
 class ProductUpdateApiView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'pk'
+    lookup_field = "pk"
 
     def perform_update(self, serializer):
         instance = serializer.save()
         if not instance.content:
             instance.content = instance.title
 
+
 class ProductDeleteApiView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'pk'
+    lookup_field = "pk"
 
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
-    
+
+
 class ProductMixinView(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
-    generics.GenericAPIView
+    generics.GenericAPIView,
 ):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes=[permissions.IsAuthenticated]
-     
-    def get(self,req,*args,**kwargs):
-        pk = kwargs.get('pk')
+    # permission_classes = [permissions.IsAuthenticated] #this will give all privilages,
+    # once user is logged In
+    # permission_classes = [
+    #     permissions.DjangoModelPermissions
+    # ]  # this will only allow those rights given in admin panel by superuser to this
+    # # user, although user can view on drf api ui, but can't view on admin panel,
+    # # to disallow the view even from api ui, we need to use django custom permission
+
+    permission_classes = [IsStaffPermission]
+
+    def get(self, req, *args, **kwargs):
+        pk = kwargs.get("pk")
         if not pk:
-            return self.list(req,*args,**kwargs)
-        return self.retrieve(req,*args,**kwargs)
+            return self.list(req, *args, **kwargs)
+        return self.retrieve(req, *args, **kwargs)
 
-    def post(self,req,*args,**kwargs):
-        return self.create(req,*args,**kwargs)
+    def post(self, req, *args, **kwargs):
+        return self.create(req, *args, **kwargs)
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
-    if request.method == 'GET':
+    if request.method == "GET":
         if pk is not None:
             product = get_object_or_404(Product, pk=pk)
             serializer = ProductSerializer(product)
             return Response(serializer.data, status=200)
-        
+
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=200)
-    
-    elif request.method == 'POST':
+
+    elif request.method == "POST":
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()  # Save the validated data
